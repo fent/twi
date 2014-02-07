3118c65d03d4b75ebe1c7bd54dad9f1f6a41f46f
Roly Fentanes <roly426@gmail.com>
2014-02-07 01:36:36 -0500

    get user via username and id

--- bin/twi.js
+++ bin/twi.js
@@ -4,8 +4,14 @@ var cmd = process.argv[2];
 function usage() {
   console.log('usage: twi <cmd> [template]');
   console.log('       twi tweet <id> [template]');
-  console.log('       twi user <screen_name> [template]');
+  console.log('       twi username <screen_name> [template]');
+  console.log('       twi userid <id> [template]');
   console.log('       twi clip [template]');
+  process.exit(1);
+}
+
+if (process.argv.length < 3) {
+  usage();
 }
 
 var commands = require('../lib/commands');
@@ -13,13 +19,11 @@ var command = commands[cmd];
 if (!Object.prototype.hasOwnProperty.call(commands, cmd)) {
   console.log('unknown command');
   usage();
-  process.exit(1);
 }
 
 if (command.length + 3 > process.argv.length) {
   console.log('not enough parameters');
   usage();
-  process.exit(1);
 }
 
 command.apply(this, process.argv.slice(3));
